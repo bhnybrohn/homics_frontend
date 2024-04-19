3adb2e8fb5b0df8f58732cb8fcea0e67badff55e
prosper jaja <jajaprosper70@gmail.com>
2024-04-19 23:50:49 +0100

    background images switch

--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -25,12 +25,12 @@ export const HeroSection = () => {
     <AnimatePresence>
       <main className="flex flex-col h-[92svh] max-[500px]:h-[50svh] max-[800px]:h-[70svh]">
         <motion.section
-          initial={{ opacity: 0 }}
+          initial={{ opacity: 1 }}
           animate={{ opacity: 1 }}
-          transition={{ delay: 0.5 }}
-          exit={{ opacity: 0, transition: { duration: 0.5 } }}
+          exit={{ opacity: 1 }}
+          transition={{ duration: 0.75 }}
           key={index}
-          className="h-full px-[clamp(2rem,4.2vw,4.2rem)] py-[clamp(3rem,7.5vw,7.5rem)]  flex items-end"
+          className="h-full px-[clamp(2rem,4.2vw,4.2rem)] py-[clamp(3rem,7.5vw,7.5rem)] flex items-end "
           style={{
             background: `url(${images[index]})`,
             backgroundRepeat: "no-repeat",
@@ -38,7 +38,7 @@ export const HeroSection = () => {
             backgroundPosition: " center",
           }}
         >
-          <figure className="max-w-[1440px] ">
+          <figure className="max-w-[1440px] p-[clamp(1rem,2vw,2rem)]">
             <div className="flex flex-col gap-4 bg-[#000]/60 rounded-2xl px-4 py-6">
               <motion.h3
                 className="text-white font-semibold max-w-[30.7rem] text-[clamp(1.5rem,2.5vw,2.5rem)]"
